9bc35dd477cf1aae5d4ac4bc1db06dd7e79bab48
Gudlajerusha <101727412+Gudlajerusha@users.noreply.github.com>
2022-03-30 15:51:20 +0530

    Add files via upload

---
+++ integers.c
@@ -0,0 +1,24 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int dig, revNumber;
+
+    printf("Enter an integer number : ");
+    scanf("%d", &n);
+
+    /*Reversing Number*/
+    revNumber = 0;
+
+    while (n > 0) {
+        dig = n % 10;
+        revNumber = (revNumber * 10) + dig;
+        n = n / 10;
+    }
+
+    printf("Reverse Number is : %d\n", revNumber);
+    return 0;
+}
