1826bdcd8243749be1a7979da7ae1230f3178ea9
Gudlajerusha <101727412+Gudlajerusha@users.noreply.github.com>
2022-03-30 15:50:28 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int i,n,sum;
+	printf("enter the value of 'n' natural numbers");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	     sum=sum+i;
+	}  
+		printf("sum=%d",sum);
+	    
+}
